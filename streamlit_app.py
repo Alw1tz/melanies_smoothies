d78d9500d31bf46d89ad95477a749bb9d770bf6e
Carlos Almeida <73144591+Alw1tz@users.noreply.github.com>
2026-03-16 19:00:30 -0600

    Update smoothie response display to use dataframe
    Comment out the text display of the smoothie response and add a dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response)
+sf_st = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     
